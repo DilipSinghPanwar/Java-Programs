de6363718a031c9c693ff528bcb8f2508220b5e7
DilipSinghPanwar <dilip.dsp@gmail.com>
2023-09-04 01:37:25 +0530

    prime number & series

---
+++ src/PrimeNumber.java
@@ -0,0 +1,24 @@
+public class PrimeNumber {
+    public static void main(String[] args) {
+        //prime, any positive integer greater than 1 that is divisible only by itself and 1
+        // e.g., 2, 3, 5, 7, 11, 13, 17, 19, 23, ….
+        // For example, 13 is only divisible by one or itself.
+        //Note: 0 and 1 are not prime numbers; 2 is the only even prime number.
+//        Why is 1 and 0 neither prime or composite?
+//        So, 0 cannot be a prime or composite number. 1 has only one factor, which is itself.
+//        It doesn't satisfy the definition of both prime and composite numbers,
+//        so it cannot be a prime or composite number. So on this basis we can easily say that “0” and “1” are neither prime nor composite.
+        int input = 13;
+        int temp = 0;
+        for (int i = 2; i < (input - 1); i++) {
+            if (input % i == 0){
+                temp = temp + 1;
+            }
+        }
+        if (temp == 0){
+            System.out.println("Input Number is Prime Number");
+        }else {
+            System.out.println("Input Number is not Prime Number");
+        }
+    }
+}
