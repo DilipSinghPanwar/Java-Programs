53c3440d77a2e28e24006248ec8b8ca400780669
DilipSinghPanwar <dilip.dsp@gmail.com>
2023-09-04 01:16:05 +0530

    program

--- src/FibonacciSeries.java
+++ src/FibonacciSeries.java
@@ -9,6 +9,8 @@ public class FibonacciSeries {
         }
     }
 
+    //Fibonacci sequence is a sequence in which each number is the sum of the two preceding ones
+    //The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
     public static int fib(int number) {
         if (number <= 1) {
             return number;
--- src/Swap2ValWithTemp.java
+++ src/Swap2ValWithTemp.java
@@ -1,5 +1,6 @@
 public class Swap2ValWithTemp {
     public static void main(String[] args) {
+        //Swapping two numbers means exchanging the values of two variables with each other.
         int first = 10;
         int second = 20;
         System.out.println("first value before swap: "+first);
